d739dd3294c6a0470eca420353018075f33e4173
take <take004@take0801.onmicrosoft.com>
2020-11-03 16:02:17 +0900

    localtunnel

---
+++ test.js
@@ -0,0 +1,16 @@
+var localtunnel = require("localtunnel");
+const { listenerCount } = require("process");
+
+try {
+  localtunnel(9080, { subdomain: "take" }, function (err, tunnel) {
+    console.log("localtunner is running");
+  });
+} catch (exception) {
+  console.log(exception);
+}
+
+/*
+forever list
+forever start test.js
+forever stopall
+*/
